54800b49d7c886469eb92b70aacfb9563492aaf0
harshitkumar31 <hrsht.kumar1@gmail.com>
2017-04-29 18:56:09 +0530

    First commit

---
+++ Saligram_API/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class SaligramApiConfig(AppConfig):
+    name = 'Saligram_API'
---
+++ authentication/urls.py
@@ -0,0 +1,13 @@
+from authentication.apiviews import AuthRegister
+
+__author__ = "Harshit"
+
+from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token, verify_jwt_token
+
+urlpatterns = [
+    url(r'^login/', obtain_jwt_token),
+    url(r'^token-refresh/', refresh_jwt_token),
+    url(r'^token-verify/', verify_jwt_token),
+    url(r'^register/$', AuthRegister.as_view()),
+]
